bf824f5041e25307f2450a85051a30a31711183d
Jerry Ma <jerry.ma.nk@gmail.com>
2020-08-31 12:50:18 -0400

    fix format for numpy

--- tollan/utils/fmt.py
+++ tollan/utils/fmt.py
@@ -205,4 +205,5 @@ def pformat_bar(
 pyaml.add_representer(np.float64, lambda s, d: s.represent_float(d))
 pyaml.add_representer(np.float32, lambda s, d: s.represent_float(d))
 pyaml.add_representer(np.int32, lambda s, d: s.represent_int(d))
+pyaml.add_representer(np.int64, lambda s, d: s.represent_int(d))
 pyaml.add_representer(None, lambda s, d: s.represent_str(str(d)))
