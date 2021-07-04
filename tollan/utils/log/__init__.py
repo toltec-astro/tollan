fe970da8ecb448807da15448a93c2a986f50f466
Jerry Ma <jerry.ma.nk@gmail.com>
2021-07-03 20:50:24 -0400

    add log to file utilities

--- tollan/utils/log/__init__.py
+++ tollan/utils/log/__init__.py
@@ -6,13 +6,15 @@ import logging.config
 import inspect
 import time
 import copy
+from pathlib import Path
+from wrapt import ObjectProxy
 from contextlib import contextmanager
 from astropy.utils.console import human_time
 from ..misc import rupdate
 from . import console_color
 
 
-__all__ = ['init_log', 'get_logger', 'timeit', 'logit']
+__all__ = ['init_log', 'log_to_file' 'get_logger', 'timeit', 'logit']
 
 
 presets = {
@@ -38,11 +40,6 @@ presets = {
                 },
             },
         'loggers': {
-            '': {
-                'handlers': ['default'],
-                'level': 'DEBUG',
-                'propagate': False
-                },
             'matplotlib': {
                 'handlers': ['default'],
                 'level': 'WARNING',
@@ -53,53 +50,58 @@ presets = {
                 'level': 'WARNING',
                 'propagate': False
                 },
-            'root': {
+            'astropy': {
                 'handlers': ['default'],
-                'level': 'ERROR',
+                'level': 'WARNING',
                 'propagate': False
                 },
-            }
+            },
+        'root': {
+            'handlers': ['default'],
+            'level': 'DEBUG',
+            'propagate': False
+            },
         },
     }
 
 
+dict_config = ObjectProxy(None)
+"""
+A proxy to the `~logging.config.dictConfigClass` instance, which is made
+available after `init_log` is called.
+"""
+
+
 def init_log(
-        preset='default',
-        level='INFO',
-        file_=None,
-        colored=True,
-        **kwargs):
+        preset='default', level='INFO',
+        filepath=None, colored=True, **kwargs):
     """Initialize logging with some sensible presets.
 
     Parameters
     ----------
-    level: str
+    preset : str
+        The predefined dict config to use.
+
+    level : str
         The log level to use.
 
-    file_: str, optional
-        If set, the log are saved in `file_`.
+    filepath : str, `pathlib.Path`, optional
+        If set, the log are saved in `filepath`.
 
-    colored: bool
+    colored : bool
         If set to True, console messages will be colored.
 
-    **kwargs:
+    **kwargs
         Update the preset with entries in this dict.
 
     """
     config = copy.deepcopy(presets[preset])
-    rupdate(
-            config,
-            {'loggers':
-                {
-                    '': {'level': level},
-                    'root': {'level': level},
-                    }})
-    if file_ is not None:
+    if filepath is not None:
         rupdate(config, {
             'handlers': {'logfile': {
                     'class': 'logging.FileHandler',
                     'formatter': "standard",
-                    'filename': file_
+                    'filename': filepath.as_posix()
                     },
                 },
             'loggers': {
@@ -108,9 +110,101 @@ def init_log(
                 },
             })
     if colored:
+        # this will replace the default handler with colored output handler
         rupdate(config, console_color.config)
+    # merge any user settings
     rupdate(config, kwargs)
-    logging.config.dictConfig(config)
+    # set levels for each handlers
+    # note we avoid setting the level for the root logger, which
+    # will override the per-handler settings
+    for h in config['handlers'].values():
+        h['level'] = level
+    # we save the config so we can access the sensibles later
+    dict_config.__wrapped__ = logging.config.dictConfigClass(config)
+    dict_config.configure()
+
+
+def _find_logfile_handler(handlers, filepath):
+    """Find from the list of handlers the one that log to given `filepath`"""
+    for handler in handlers:
+        if isinstance(handler, logging.FileHandler):
+            if Path(handler.baseFilename).samefile(filepath):
+                return handler
+    return None
+
+
+def add_logfile_handler(
+        logger, filepath, level='DEBUG', formatter=None):
+    """Add a handler to logger to log to `filepath`."""
+    filepath = Path(filepath)
+    h = _find_logfile_handler(logger.handlers, filepath)
+    if h is None:
+        # create logfile handler
+        if formatter is None:
+            formatter = presets['default']['formatters']['standard']
+        if isinstance(formatter, dict):
+            f = dict_config.configure_formatter(formatter)
+        else:
+            f = formatter
+        h = logging.FileHandler(
+                filename=filepath.as_posix(),
+                )
+        h.setFormatter(f)
+        logger.addHandler(h)
+    return h
+
+
+def remove_logfile_handler(logger, filepath, level='DEBUG'):
+    """Remove the handler from logger to log to `filepath`."""
+    filepath = Path(filepath)
+    h = _find_logfile_handler(logger.handlers, filepath)
+    if h is not None:
+        logger.removeHandler(h)
+
+
+class log_to_file(AbstractContextManager):
+    """A context manager to log to file.
+
+    Parameters
+    ----------
+    filepath : str, `pathlib.Path`, optional
+        The log file path.
+    logger : `logging.Logger`, optional
+        The logger to log to file. If None, the root logger is used.
+    level : str
+        The log level.
+    disable_other_handlers : bool
+        If True, other handlers will be disabled. It will be restored
+        at exit.
+    """
+
+    def __init__(
+            self, filepath, logger=None, level='DEBUG',
+            disable_other_handlers=False):
+        self._filepath = Path(filepath)
+        self._logger = logger or get_logger('')
+        self._level = level
+        self._disable_other_handlers = disable_other_handlers
+
+    def __enter__(self):
+        logger = self._logger
+        logfile_handler = self._logfile_handler = add_logfile_handler(
+                logger=logger, filepath=self._filepath,
+                level=self._level)
+        if self._disable_other_handlers:
+            original_handlers = self._original_handlers = list()
+            for h in logger.handlers:
+                if h is not logfile_handler:
+                    original_handlers.append(h)
+            for h in original_handlers:
+                logger.removeHandler(h)
+
+    def __exit__(self, *exc_info):
+        logger = self._logger
+        logger.removeHandler(self._logfile_handler)
+        if self._disable_other_handlers:
+            for h in self._original_handlers:
+                self._logger.addHandler(h)
 
 
 def get_logger(name=None):
@@ -218,15 +312,15 @@ def disable_logger(*names):
             logger.disabled = old_values[logger]
 
 
-def logged_dict_update(log_func, l, r):
+def logged_dict_update(log_func, left, right):
     """Update a dict with any changes tracked via `log_func`."""
-    for k, v in r.items():
-        if k in l and l[k] != v:
+    for k, v in right.items():
+        if k in left and left[k] != v:
             log_func(
                     f"entry changed"
-                    f" {k} {l[k]} -> {v}")
-        l[k] = v
-    return l
+                    f" {k} {left[k]} -> {v}")
+        left[k] = v
+    return left
 
 
 class logged_closing(AbstractContextManager):
