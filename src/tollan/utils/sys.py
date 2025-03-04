f5f51eec32757bf1340433f9afa6c1edf9e7ca13
Jerry <jerry.ma.nk@gmail.com>
2025-03-04 01:42:10 -0500

    add path utility

--- src/tollan/utils/sys.py
+++ src/tollan/utils/sys.py
@@ -1,3 +1,4 @@
+import contextlib
 import os
 import pty
 import pwd
@@ -5,16 +6,18 @@ import shlex
 import socket
 from pathlib import Path
 
-from .log import logger
+from .log import logger, logit
 
 """System related helpers."""
 
 
 __all__ = [
-    "get_username",
+    "ensure_path_parent_exists",
     "get_hostname",
-    "touch_file",
     "get_or_create_dir",
+    "get_username",
+    "pty_run",
+    "touch_file",
 ]
 
 
@@ -35,7 +38,7 @@ def touch_file(out_file):
         os.utime(out_file, None)
 
 
-def get_or_create_dir(dirpath, on_exist=None, on_create=None):
+def get_or_create_dir(dirpath, on_exist=None, on_create=None, on_create_context=None):
     """Ensure `dirpath` exist.
 
     Parameters
@@ -46,16 +49,31 @@ def get_or_create_dir(dirpath, on_exist=None, on_create=None):
         If set, called if `dirpath` exists already.
     on_create : callable, optional
         If set, called if `dirpath` is created.
+    on_create_context : context, optional
+        The context to enter on create.
     """
     dirpath = Path(dirpath)
     if dirpath.exists():
         if on_exist is not None:
             on_exist(dirpath)
         return dirpath
-    dirpath.mkdir(parents=True)
+    with on_create_context or contextlib.nullcontext():
+        dirpath.mkdir(parents=True)
     if on_create is not None:
         on_create(dirpath)
     return dirpath
+
+
+def ensure_path_parent_exists(path: Path):
+    """Ensure parent path of `path` exists."""
+    if path.is_dir():
+        raise ValueError("invalid path type.")
+    parent = path.parent
+    get_or_create_dir(
+        parent,
+        on_create_context=logit(logger.info, f"create dir {parent}"),
+    )
+    return path
 
 
 def pty_run(cmd):
